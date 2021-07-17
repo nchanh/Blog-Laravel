f8eccefbf905faf0b33d8870975f8f22f448f6dc
Nguyen Chanh <nguyenchanh0306@gmail.com>
2021-07-17 12:08:39 +0700

    chore: edit code in home page

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -14,14 +14,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if (Auth::user()->is_admin()){
-            //fetch 5 posts from database which are latest
-            $posts = Posts::orderBy('created_at','desc')->paginate(5);
-        }
-        else {
-            //fetch 5 posts from database which are active and latest
-            $posts = Posts::where('active', 1)->orderBy('created_at','desc')->paginate(5);
-        }
+        //fetch 5 posts from database which are active and latest
+        $posts = Posts::where('active', 1)->orderBy('created_at','desc')->paginate(5);
 
         // redirect view home and data posts
         return view('home')->withPosts($posts);
